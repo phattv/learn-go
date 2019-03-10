0cd50fa8d9fa270bdea46316120f64ed666c1bbd
Phat Tran <phattv.dev@gmail.com>
2019-03-11 00:22:58 +0800

    Variables

--- main.go
+++ main.go
@@ -5,9 +5,15 @@ import (
 )
 
 func main() {
-	helloWorld()
+	// helloWorld()
+	sum(3, 4)
 }
 
 func helloWorld() {
 	fmt.Println("Hello, World!")
+}
+
+func sum(a, b int) {
+	sum := a + b
+	fmt.Println(sum)
 }
